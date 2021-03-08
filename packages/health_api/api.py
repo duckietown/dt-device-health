f16d19cd00f153523f2a48ae1f098cd1ff42c87c
Andrea F. Daniele <afdaniele@ttic.edu>
2021-03-08 14:53:28 -0600

    added /trigger/X API endpoint

--- packages/health_api/api.py
+++ packages/health_api/api.py
@@ -1,6 +1,10 @@
-from flask import Flask, Blueprint, abort, jsonify
+import uuid
+
+from flask import Flask, Blueprint, abort, jsonify, request
 from flask_cors import CORS
 
+from dt_triggers_utils import set_trigger
+from health_api.knowledge_base import KnowledgeBase
 from health_api.resources import all_resources, cached_resource
 
 
@@ -18,6 +22,9 @@ class HealthAPI(Flask):
         super(HealthAPI, self).__init__(__name__)
         # keep a link to the Battery object
         __battery__ = app.battery
+        # create token for privileged operations
+        KnowledgeBase.set('trigger/shutdown', str(uuid.uuid4()), ttl=-1)
+        KnowledgeBase.set('trigger/reboot', str(uuid.uuid4()), ttl=-1)
         # register blueprints
         self.register_blueprint(api)
         # apply CORS settings
@@ -34,6 +41,23 @@ def _all():
         value = cached_resource(resource)
         res.update(value)
     return jsonify(res)
+
+
+@api.route('/trigger/<string:trigger>')
+def _trigger(trigger: str):
+    if not KnowledgeBase.has(f'trigger/{trigger}'):
+        return jsonify({'status': 'error', 'message': f"Trigger '{trigger}' not supported"})
+    # check token or provide one if wrong/none was given
+    right_token = KnowledgeBase.get(f'trigger/{trigger}')
+    given_token = request.args.get('token', default=None)
+    if given_token != right_token:
+        return jsonify({'status': 'needs-confirmation', 'token': right_token})
+    # set trigger
+    try:
+        set_trigger(trigger, 'health-api')
+    except FileNotFoundError as e:
+        return jsonify({'status': 'error', 'message': str(e)})
+    return jsonify({'status': 'ok'})
 
 
 @api.route('/battery/history')
