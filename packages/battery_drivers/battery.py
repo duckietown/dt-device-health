67c494a7bdf39e0e3090a3eeb757033373274108
Andrea F. Daniele <afdaniele@ttic.edu>
2021-03-17 17:12:46 -0500

    improved battery drivers, added option to send commands to the battery

--- packages/battery_drivers/battery.py
+++ packages/battery_drivers/battery.py
@@ -1,3 +1,4 @@
+import re
 import time
 import traceback
 from logging import Logger
@@ -8,7 +9,7 @@ import yaml
 from serial.tools.list_ports import grep as serial_grep
 
 from dt_class_utils import DTReminder
-from .constants import BATTERY_VID, BATTERY_PID
+from .constants import BATTERY_PCB16_READY_VID, BATTERY_PCB16_READY_PID, BATTERY_PCB16_BAUD_RATE
 from .history import BatteryHistory
 
 KELVIN_TO_CELSIUS = lambda k: k - 273.15
@@ -35,10 +36,10 @@ class Battery:
 
     def __init__(self, callback, logger: Logger = None):
         self._devices = []
-        self._baud_rate = 9600
         self._info = None
         self._data = None
         self._device = None
+        self._command = None
         self._is_shutdown = False
         self._logger = logger
         if not callable(callback):
@@ -48,15 +49,28 @@ class Battery:
         self._worker = Thread(target=self._work)
         self._history = BatteryHistory()
 
-    def start(self):
-        self._worker.start()
+    def start(self, block: bool = False, quiet: bool = True):
+        if block:
+            return self._work(quiet=quiet)
+        else:
+            self._worker.start()
 
     def join(self):
-        self._worker.join()
+        if self._worker.is_alive():
+            self._worker.join()
+
+    def is_shutdown(self):
+        return self._is_shutdown
 
     def shutdown(self):
+        #   This is NOT a battery shutdown, it simply shuts down the drivers
         self._is_shutdown = True
-        self._worker.join()
+        self.join()
+
+    def turn_off(self, timeout: int = 20):
+        #   This is a battery shutdown, the power will be cut off after `timeout` seconds
+        timeout = f'{timeout}'.zfill(2)
+        self._command = f'Q{timeout}'.encode('utf-8')
 
     @property
     def info(self):
@@ -75,7 +89,8 @@ class Battery:
         return self._history.get()
 
     def _find_device(self):
-        ports = serial_grep("VID:PID={}:{}".format(BATTERY_VID, BATTERY_PID))
+        vid_pid_match = "VID:PID={}:{}".format(BATTERY_PCB16_READY_VID, BATTERY_PCB16_READY_PID)
+        ports = serial_grep(vid_pid_match)
         self._devices = [p.device for p in ports]  # ['/dev/ttyACM0', ...]
 
     def _chew_on_data(self):
@@ -83,10 +98,11 @@ class Battery:
             self._callback(self._data)
             self._history.add(self._data)
 
-    def _read_next(self, dev):
+    def _read_next(self, dev, quiet: bool = True):
         try:
             raw = dev.read_until().decode('utf-8')
-            cleaned = raw.replace('\x00 ', '').replace('\x00', '').rstrip()
+            cleaned = re.sub(r"\x00\s*", "", raw).rstrip()
+            cleaned = re.sub(r"-\s+", "-", cleaned)
             try:
                 parsed = yaml.load(cleaned, yaml.SafeLoader)
                 return parsed
@@ -94,10 +110,13 @@ class Battery:
                 if self._logger:
                     self._logger.error(str(e))
                 return None
-        except BaseException:
-            traceback.print_exc()
+        except BaseException as e:
+            if quiet:
+                traceback.print_exc()
+            else:
+                raise e
 
-    def _work(self):
+    def _work(self, quiet: bool = True):
         while True:
             if self._is_shutdown:
                 return
@@ -111,20 +130,27 @@ class Battery:
             else:
                 # we have at least one candidate device, try reading
                 for device in self._devices:
-                    with serial.Serial(device, self._baud_rate) as dev:
+                    with serial.Serial(device, BATTERY_PCB16_BAUD_RATE) as dev:
                         # once the device is open, try reading from it forever
                         # break only on unknown errors
                         while True:
                             if self._is_shutdown:
                                 return
                             # ---
+                            if self._command is not None:
+                                # there is a command to be sent, send it and continue
+                                for _ in range(3):
+                                    dev.write(self._command)
+                                    dev.flush()
+                                self._command = None
+                                continue
                             if self._data is not None and self._info is None:
                                 # we were able to read from the battery at least once, request ??
                                 dev.write(b'??')
                                 dev.flush()
                             # ---
                             try:
-                                line = self._read_next(dev)
+                                line = self._read_next(dev, quiet=quiet)
                                 if line is None:
                                     continue
                                 # first time we read?
@@ -136,9 +162,11 @@ class Battery:
                                     self._info = self._format_info(line)
                                 elif 'SOC(%)' in line:
                                     self.data = self._format_data(line)
-                            except BaseException:
-                                traceback.print_exc()
-                                break
+                            except BaseException as e:
+                                if quiet:
+                                    traceback.print_exc()
+                                    break
+                                raise e
             if self._logger:
                 self._logger.warning('An error occurred while reading from the battery.')
             # allow 5 seconds for things to reset
